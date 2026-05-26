0bf86f06f009b8d569d0ada2846d8401bb7d0e9a
sit2606 <sit2606@gmail.com>
2026-05-26 16:45:58 +0300

    vault backup: 2026-05-26 16:45:58

---
+++ 02. Введение в базы данных/Практика/Лабораторная 6/create_database.sql
@@ -0,0 +1,9 @@
+Create database "ApressFinancial"
+with 
+owner = postgres
+encoding = 'UTF8'
+LC_COLLATE = 'Russian_Russia.1251'
+LC_CTYPE = 'Russian_Russia.1251'
+tablespace = pg_default
+CONNECTION LIMIT = 1
+IS_TEMPLATE = False;
