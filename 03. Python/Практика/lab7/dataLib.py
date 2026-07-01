a82de487b0b4538e8d1e61c135c31cbef3bcaf7b
sit2606 <sit2606@gmail.com>
2026-07-01 12:57:43 +0300

    vault backup: 2026-07-01 12:57:42

---
+++ 03. Python/Практика/lab7/dataLib.py
@@ -0,0 +1,12 @@
+def create_market():
+    pass
+def update_market():
+    pass
+def delete_market():
+    pass
+def create_reference():
+    pass
+def update_reference():
+    pass
+def delete_reference():
+    pass
