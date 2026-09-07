ddeed79f59fec3dc62bef158c9eb9b33a0df1dc9
sit2606 <sit2606@gmail.com>
2026-09-07 18:00:17 +0300

    Delete stupid AI folder

--- 03. Python/ПRACTICA/lab10/run_tests.py
+++
@@ -1,7 +0,0 @@
-import unittest
-
-loader = unittest.TestLoader()
-suite = loader.discover(start_dir='tests', pattern='test_*.py')
-
-runner = unittest.TextTestRunner(verbosity=2)
-runner.run(suite)
